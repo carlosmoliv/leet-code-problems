5999e42c73a58132ef030ea1b31c97b8fc574df2
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-03-27 21:20:31 -0300

    refactor: improve topKFrequent code

--- src/arrays-and-hashing/top-k-frequent-elements.spec.ts
+++ src/arrays-and-hashing/top-k-frequent-elements.spec.ts
@@ -1,12 +1,11 @@
-// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 function topKFrequent(nums: number[], k: number): number[] {
   const map = new Map<number, number>();
   for (const num of nums) {
-    const count = map.get(num) || 0;
-    map.set(num, count + 1);
+    let elementCount = map.get(num) || 0;
+    map.set(num, elementCount + 1);
   }
-  const entries = Array.from(map.entries()).sort((a, b) => b[1] - a[1]);
-  return entries.slice(0, k).map(([num]) => num);
+  const sortedDescending = [...map.entries()].sort((a, b) => b[1] - a[1]);
+  return sortedDescending.slice(0, k).map((element) => element[0]);
 }
 
 describe("Top K Frequent", () => {
@@ -14,7 +13,7 @@ describe("Top K Frequent", () => {
     [[1, 1, 1, 2, 2, 3], 2, [1, 2]],
     [[1], 1, [1]],
   ])(
-    "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.",
+    "Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order",
     (numsInput, kInput, expectedResult) => {
       expect(topKFrequent(numsInput, kInput)).toEqual(expectedResult);
     }
